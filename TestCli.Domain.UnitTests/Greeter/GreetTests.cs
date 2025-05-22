0c240913528c4dea5256b6e04d5a5cc7f88cf182
James.Webber <James.Webber@advancedsecure.co.uk>
2025-05-22 09:34:28 +0100

    Add project files.

---
+++ TestCli.Domain.UnitTests/Greeter/GreetTests.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TestCli.Domain.Greeter;
+
+namespace TestCli.Domain.UnitTests.Greeter;
+public class GreetTests
+{
+    private readonly GreeterService _sut = new GreeterService();
+
+    [Fact]
+    public void GivenFriendlyGreetingType_ThenReturnsFriendlyGreeting()
+    {
+        // arrange
+        var request = new GreetRequest
+        {
+            GreetingType = GreetingType.Friendly,
+            Name = "James",
+        };
+
+        // act
+        var response = _sut.Greet(request);
+
+        // assert
+        Assert.Equal("Hello, James!", response);
+    }
+
+    [Fact]
+    public void GivenRudeGreetingType_ThenReturnsRudeGreeting()
+    {
+        // arrange
+        var request = new GreetRequest
+        {
+            GreetingType = GreetingType.Rude,
+            Name = "James",
+        };
+
+        // act
+        var response = _sut.Greet(request);
+
+        // assert
+        Assert.Equal("Go away, James!!", response);
+    }
+
+    [Theory]
+    [InlineData(GreetingType.Friendly, null)]
+    [InlineData(GreetingType.Friendly, "")]
+    [InlineData(GreetingType.Friendly, " ")]
+    [InlineData(GreetingType.Friendly, "          ")]
+    public void GivenBadRequest_ThrowsValidationExceptionWithExpectedMessage(GreetingType greetingType, string? name)
+    {
+        // arrange
+        var request = new GreetRequest
+        {
+            GreetingType = greetingType,
+            Name = name,
+        };
+
+        // act
+        Func<string> act = () => _sut.Greet(request);
+
+        // assert
+        Assert.Multiple(() =>
+        {
+            var caughtException = Assert.Throws<ValidationException>(act);
+        });
+    }
+}
---
+++ TestCli.Domain/Greeter/GreeterService.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestCli.Domain.Greeter;
+public class GreeterService
+{
+    public string Greet(GreetRequest request)
+    {
+        // validate and throw if invalid
+        Validator.ValidateObject(request, new ValidationContext(request), validateAllProperties: true);
+
+        if (request.GreetingType == GreetingType.Friendly)
+        {
+            return $"Hello, {request.Name}!";
+        }
+
+        return $"Go away, {request.Name}!!";
+    }
+}
+
+public record GreetRequest
+{
+    public GreetingType GreetingType { get; init; }
+
+    [Required]
+    public string Name { get; init; } = string.Empty;
+}
+
+public enum GreetingType
+{
+    Friendly,
+    Rude,
+}
